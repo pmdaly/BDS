51cc340fb74bfb3235ffe7f48b6b8871f8d0c4de
Patrick Daly <pmdaly12@gmail.com>
2020-03-20 13:56:38 -0700

    wavelet processing added

---
+++ bds/signal_processing/wavelet.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pywt
+from pathos.multiprocessing import Pool
+
+
+class WaveletReduce:
+
+    @staticmethod
+    def get_wavelets(df, fs, n_jobs=20):
+        try:
+            signals = df.values
+        except AttributeError:
+            signals = df
+        num_signals = signals.shape[1]
+        power_envs = None
+        with Pool(n_jobs) as p:
+            pool_items = ({'elec': signals[:, elec], 'fs': fs} for elec in range(num_signals))
+            power_envs = np.array(list(p.imap(WaveletReduce._process_elec, pool_items)))
+        return power_envs
+
+    @staticmethod
+    def _process_elec(args):
+        elec_data = args['elec']
+        fs = args['fs']
+        elec_envs = list()
+        scales = np.logspace(np.log10(1), np.log10(150), 25)
+        f = pywt.scale2frequency(wavelet='morl', scale=scales) * fs
+        for sig in chunker(elec_data, 30*fs):
+            coefs, freqs = pywt.cwt(sig, f, 'morl', sampling_period=1/fs)
+            amps = np.abs(coefs)
+            delta   = amps[np.where((freqs >=  1) & (freqs <  4))[0], :].mean()
+            theta   = amps[np.where((freqs >=  4) & (freqs <  9))[0], :].mean()
+            alpha   = amps[np.where((freqs >=  9) & (freqs < 13))[0], :].mean()
+            beta    = amps[np.where((freqs >= 13) & (freqs < 31))[0], :].mean()
+            gamma_l = amps[np.where((freqs >= 31) & (freqs < 81))[0], :].mean()
+            gamma_h = amps[np.where(freqs >= 81)[0], :].mean()
+            elec_envs.append([delta, theta, alpha, beta, gamma_l, gamma_h])
+        return elec_envs
+
+
+def chunker(iterable, chunk_size, dim=0):
+    '''Returns a generator with chunk_size chunks'''
+    if dim == 0:
+        for chunk in range(0, len(iterable), chunk_size):
+            yield iterable[chunk: chunk + chunk_size]
+    else:
+        for chunk in range(0, len(iterable[0]), chunk_size):
+            yield iterable[:, chunk: chunk + chunk_size]
--- scripts/testing.py
+++ scripts/testing.py
@@ -1,5 +1,7 @@
 from bds.seeg_models import Patient, Population
 from bds.signal_estimation import Estimator
+from bds.signal_processing import WaveletReduce
+
 
 patients = [Patient() for _ in range(5)]
 population = Population(patients)
@@ -8,4 +10,8 @@ population.set_patient_models()
 population.build_full_model()
 
 est = Estimator(population.model)
-p1_full = est.estimate(population[0])
+p1_bos_full = est.estimate(population[0])
+
+spectra = list()
+for bo_full in p1_bos_full:
+    spectra.append(WaveletReduce().get_wavelts(bo_full.get_data(), 512))
